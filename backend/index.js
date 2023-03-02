a937cdb7ad587a7611e5ecc87e806be5ad743ff7
Pavan Jadhav <114083627+IAMPAVANJ@users.noreply.github.com>
2023-03-02 16:14:24 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const conn = require("./Database/db");
 const express= require("express");
+const cors = require("cors");
+const bodyParser = require('body-parser');
 const app = express();
 let port =process.env.PORT ||  8080;
 conn();
 const registerRoute = require("./routes/register");
 const loginRoute = require("./routes/login");
 const createSurveyRoute = require("./routes/createSurvey");
+const  routerTheme = require("./themes/themes")
 
+app.use(bodyParser.json());
+app.use(routerTheme)
 app.use(registerRoute);
 app.use(loginRoute);
 app.use(createSurveyRoute);
