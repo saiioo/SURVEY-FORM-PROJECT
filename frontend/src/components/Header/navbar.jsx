253ad0e65bcc776e8a62afed1eab1ff21c490619
Abhishek Gupta <abskgupta11@gmail.com>
2023-03-04 16:57:51 +0530

    FInal-touch

--- frontend/src/components/Header/navbar.jsx
+++ frontend/src/components/Header/navbar.jsx
@@ -1,7 +1,9 @@
 import { FaBookOpen } from "react-icons/fa";
 import ThemeDropDown from "../ThemesDropDownMenu/themeDropDown"
 import "./navbar.css"
-
+import Logout from "./logout";
+import localname from "./localname";
+import { AiOutlineUser } from "react-icons/ai";
 const Navbar = ()=>{
 
     return(
@@ -11,8 +13,10 @@ const Navbar = ()=>{
             </div>
             <div  className="userInfo">
             <div className="userImage"></div>
-                <p>Pavan jadhav</p>
+            <p className="username" ><AiOutlineUser color="green"  size="25px"/><b>{localname()}</b></p>
+                
             </div>
+            <span className="Logout"><Logout/></span>
                 <ThemeDropDown/>
         </nav>
     )
